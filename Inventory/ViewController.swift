2782e37e81e931cbb3003d0fd7b3793feee05686
Weston Verhulst <w2visme@gmail.com>
2019-04-15 11:02:23 -0500

    Initial Commit

---
+++ Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Weston Verhulst on 4/15/19.
+//  Copyright © 2019 Weston Verhulst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
